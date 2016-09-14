636957143c69be010440e82d844732062fbef730
Oates, Dr. Richard <Richard.Oates@comdirect.de>
2016-09-14 08:05:46 +0200

    initial commit, spring-boot dependencies

---
+++ src/main/java/de/comdirect/collabothon/HelloController.java
@@ -0,0 +1,14 @@
+package de.comdirect.collabothon;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+   @RequestMapping("/")
+   public String index() {
+      return "Greetings from Spring Boot!";
+   }
+
+}
