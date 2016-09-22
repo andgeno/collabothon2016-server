c41934aa1b5e071fcab9eb7a72609cff5c375e66
A4595294 <A4595294@NOT-0406>
2016-09-22 22:27:39 +0200

    Adding Suggestions

--- voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
+++ voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
@@ -1,23 +1,27 @@
 package de.comdirect.collabothon2016.voting;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import de.comdirect.collabothon2016.data.InvestmentSuggestion;
 
 public class Fallback {
 
-	public static List<InvestmentSuggestion> getInvestmentSuggestion()
+	public static HashMap<Long,List<InvestmentSuggestion>> getInvestmentSuggestion()
 	{
 		InvestmentSuggestion suggestion = new InvestmentSuggestion();
 		suggestion.setTitle("In Edelholz investieren");
 		suggestion.setComment("Für eine langfristige und sachwertorientierte Edelholzgeldanlage. Das erklärte Ziel ist es, neben überdurchschnittlichen Erträgen ein Maximum an Umweltschutz, Ökologie und Klimaschutz zu berücksichtigen, um einen Mehrwert für Sie und unsere Erde zu schaffen");
 		suggestion.setUserId(1);
+		suggestion.setVotes(3);
 		
 		List<InvestmentSuggestion> investmentSuggestions = new ArrayList<>();
-		investmentSuggestions.add(suggestion);
+		HashMap<Long,List<InvestmentSuggestion>> s = new HashMap<Long,List<InvestmentSuggestion>>();
 		
-		return investmentSuggestions;
+		investmentSuggestions.add(suggestion);
+		s.put(1000L, investmentSuggestions);
+		return s;
 	}
 	
 }
