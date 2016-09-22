7794054923c46f4313ea50d8b66fd7df3460ffe6
A4595294 <A4595294@NOT-0406>
2016-09-22 17:50:04 +0200

    Fallback für Depotanfrage

---
+++ depotengine/src/main/java/de/comdirect/collabothon2016/depotengine/data/Fallback.java
@@ -0,0 +1,26 @@
+package de.comdirect.collabothon2016.depotengine.data;
+
+
+public class Fallback {
+
+	public Portfolio getFallback()
+	{
+		Portfolio portfolio = new Portfolio();
+		
+		portfolio.setCapitalAccountNumber("1182000001");
+		portfolio.setPortfolioNumber("118200000100");
+		portfolio.setCustomerNumber("118200000140");
+		
+		portfolio.add(createPostion(100, "710000"));
+		
+		return portfolio;
+	}
+
+	private PortfolioPosition createPostion( long amount, String wkn) {
+		PortfolioPosition portfolioPosition = new PortfolioPosition();
+		portfolioPosition.setAmount(amount);
+		portfolioPosition.setWkn(wkn);
+		return portfolioPosition;
+	}
+	
+}
