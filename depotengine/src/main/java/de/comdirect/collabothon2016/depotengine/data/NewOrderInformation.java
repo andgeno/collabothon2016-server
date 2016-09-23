c3c77609fed5dfc621b8dd853b00ca333e6105f8
A4595419 <A4595419@NOT-0445>
2016-09-23 03:08:10 +0200

    NewOrderInformation for create a new order

--- depotengine/src/main/java/de/comdirect/collabothon2016/depotengine/data/NewOrderInformation.java
+++ depotengine/src/main/java/de/comdirect/collabothon2016/depotengine/data/NewOrderInformation.java
@@ -2,4 +2,8 @@ package de.comdirect.collabothon2016.depotengine.data;
 
 public class NewOrderInformation {
 
+	private String orderTyp = "Einzelorder";
+	private String depotnr;
+	private String geschaeftsart = "K";
+	private String wertpapierId = "";
 }
