7d8ca2b8c6b308c72be297eda2f6b579e31b09e3
A4595294 <A4595294@NOT-0406>
2016-09-23 13:04:47 +0200

    Buck in den Empfehlungen behoben

--- voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
+++ voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
@@ -11,9 +11,9 @@ public class Fallback {
 
 	public static HashMap<Long,List<InvestmentSuggestion>> getInvestmentSuggestion()
 	{
-		InvestmentSuggestion suggestion = new InvestmentSuggestion();
 		List<InvestmentSuggestion> investmentSuggestions = new ArrayList<>();
 
+		InvestmentSuggestion suggestion = new InvestmentSuggestion();
 		suggestion.setTitle("Facebook Inc.");
 		suggestion.setWertpapier("US30303M1027");
 		suggestion.setComment("Analysts: 28/30 in favor");
@@ -21,40 +21,45 @@ public class Fallback {
 		suggestion.setVotes(2);
 		investmentSuggestions.add(suggestion);
 
-		suggestion.setTitle("Netflix Inc.");
-		suggestion.setWertpapier("US64110L1061");
-		suggestion.setComment("CNBC: Stock doubling in 3 years?");
-		suggestion.setUserId(3);
-		suggestion.setVotes(1);
-		investmentSuggestions.add(suggestion);
+		InvestmentSuggestion suggestion1 = new InvestmentSuggestion();
+		suggestion1.setTitle("Netflix Inc.");
+		suggestion1.setWertpapier("US64110L1061");
+		suggestion1.setComment("CNBC: Stock doubling in 3 years?");
+		suggestion1.setUserId(3);
+		suggestion1.setVotes(1);
+		investmentSuggestions.add(suggestion1);
 	
-		suggestion.setTitle("Tesla");
-		suggestion.setWertpapier("US88160R1014");
-		suggestion.setComment("Solar roof brings new potential!");
-		suggestion.setUserId(1);
-		suggestion.setVotes(1);
-		investmentSuggestions.add(suggestion);
+		InvestmentSuggestion suggestion2 = new InvestmentSuggestion();
+		suggestion2.setTitle("Tesla");
+		suggestion2.setWertpapier("US88160R1014");
+		suggestion2.setComment("Solar roof brings new potential!");
+		suggestion2.setUserId(1);
+		suggestion2.setVotes(1);
+		investmentSuggestions.add(suggestion2);
 		
-		suggestion.setTitle("GoPro");
-		suggestion.setWertpapier("US88160R1014");
-		suggestion.setComment("Technical long momentum impulse");
-		suggestion.setUserId(2);
-		suggestion.setVotes(1);
-		investmentSuggestions.add(suggestion);
+		InvestmentSuggestion suggestion3 = new InvestmentSuggestion();
+		suggestion3.setTitle("GoPro");
+		suggestion3.setWertpapier("US88160R1014");
+		suggestion3.setComment("Technical long momentum impulse");
+		suggestion3.setUserId(2);
+		suggestion3.setVotes(1);
+		investmentSuggestions.add(suggestion3);
 		
-		suggestion.setTitle("Alphabet Inc. (A)");
-		suggestion.setWertpapier("US02079K3059");
-		suggestion.setComment("Portfolio improvement through AirBNB investment");
-		suggestion.setUserId(3);
-		suggestion.setVotes(1);
-		investmentSuggestions.add(suggestion);
+		InvestmentSuggestion suggestion4 = new InvestmentSuggestion();
+		suggestion4.setTitle("Alphabet Inc. (A)");
+		suggestion4.setWertpapier("US02079K3059");
+		suggestion4.setComment("Portfolio improvement through AirBNB investment");
+		suggestion4.setUserId(3);
+		suggestion4.setVotes(1);
+		investmentSuggestions.add(suggestion4);
 		
-		suggestion.setTitle("Apple Inc. (A)");
-		suggestion.setWertpapier("US0378331005");
-		suggestion.setComment("Always good");
-		suggestion.setUserId(3);
-		suggestion.setVotes(1);
-		investmentSuggestions.add(suggestion);
+		InvestmentSuggestion suggestion5 = new InvestmentSuggestion();
+		suggestion5.setTitle("Apple Inc. (A)");
+		suggestion5.setWertpapier("US0378331005");
+		suggestion5.setComment("Always good");
+		suggestion5.setUserId(3);
+		suggestion5.setVotes(1);
+		investmentSuggestions.add(suggestion5);
 		
 		HashMap<Long,List<InvestmentSuggestion>> s = new HashMap<Long,List<InvestmentSuggestion>>();
 		
