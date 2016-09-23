1198a8d9981e79ae6897249f9a91ff141ce85723
A4595294 <A4595294@NOT-0406>
2016-09-23 12:24:55 +0200

    Static data erweitert. Leaderboard angefangen.

--- voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
+++ voting/src/main/java/de/comdirect/collabothon2016/voting/Fallback.java
@@ -5,23 +5,89 @@ import java.util.HashMap;
 import java.util.List;
 
 import de.comdirect.collabothon2016.data.InvestmentSuggestion;
+import de.comdirect.collabothon2016.data.LeaderBoardEntry;
 
 public class Fallback {
 
 	public static HashMap<Long,List<InvestmentSuggestion>> getInvestmentSuggestion()
 	{
 		InvestmentSuggestion suggestion = new InvestmentSuggestion();
-		suggestion.setTitle("In Edelholz investieren");
-		suggestion.setComment("Für eine langfristige und sachwertorientierte Edelholzgeldanlage. Das erklärte Ziel ist es, neben überdurchschnittlichen Erträgen ein Maximum an Umweltschutz, Ökologie und Klimaschutz zu berücksichtigen, um einen Mehrwert für Sie und unsere Erde zu schaffen");
-		suggestion.setUserId(1);
-		suggestion.setVotes(3);
-		
 		List<InvestmentSuggestion> investmentSuggestions = new ArrayList<>();
+
+		suggestion.setTitle("Facebook Inc.");
+		suggestion.setWertpapier("US30303M1027");
+		suggestion.setComment("Analysts: 28/30 in favor");
+		suggestion.setUserId(1);
+		suggestion.setVotes(2);
+		investmentSuggestions.add(suggestion);
+
+		suggestion.setTitle("Netflix Inc.");
+		suggestion.setWertpapier("US64110L1061");
+		suggestion.setComment("CNBC: Stock doubling in 3 years?");
+		suggestion.setUserId(3);
+		suggestion.setVotes(1);
+		investmentSuggestions.add(suggestion);
+	
+		suggestion.setTitle("Tesla");
+		suggestion.setWertpapier("US88160R1014");
+		suggestion.setComment("Solar roof brings new potential!");
+		suggestion.setUserId(1);
+		suggestion.setVotes(1);
+		investmentSuggestions.add(suggestion);
+		
+		suggestion.setTitle("GoPro");
+		suggestion.setWertpapier("US88160R1014");
+		suggestion.setComment("Technical long momentum impulse");
+		suggestion.setUserId(2);
+		suggestion.setVotes(1);
+		investmentSuggestions.add(suggestion);
+		
+		suggestion.setTitle("Alphabet Inc. (A)");
+		suggestion.setWertpapier("US02079K3059");
+		suggestion.setComment("Portfolio improvement through AirBNB investment");
+		suggestion.setUserId(3);
+		suggestion.setVotes(1);
+		investmentSuggestions.add(suggestion);
+		
+		suggestion.setTitle("Apple Inc. (A)");
+		suggestion.setWertpapier("US0378331005");
+		suggestion.setComment("Always good");
+		suggestion.setUserId(3);
+		suggestion.setVotes(1);
+		investmentSuggestions.add(suggestion);
+		
 		HashMap<Long,List<InvestmentSuggestion>> s = new HashMap<Long,List<InvestmentSuggestion>>();
 		
-		investmentSuggestions.add(suggestion);
-		s.put(1000L, investmentSuggestions);
+		s.put(1L, investmentSuggestions);
 		return s;
+	}
+
+	public static HashMap<Long, List<LeaderBoardEntry>> getLeaderBoard() {
+		LeaderBoardEntry entry = new LeaderBoardEntry();
+		List<LeaderBoardEntry> leaderBoard = new ArrayList<>();
+
+		entry.setNutzerId(1);
+		entry.setPerformance(12.21);
+		entry.setPosition(0);
+		leaderBoard.add(entry);
+
+		entry.setNutzerId(2);
+		entry.setPerformance(9.7);
+		entry.setPosition(1);
+		leaderBoard.add(entry);
+
+		entry.setNutzerId(3);
+		entry.setPerformance(-2.3);
+		entry.setPosition(3);
+		leaderBoard.add(entry);
+		
+		HashMap<Long,List<LeaderBoardEntry>> allBoards = new HashMap<Long,List<LeaderBoardEntry>>();
+		
+		allBoards.put(0L, leaderBoard);
+		
+		
+		
+		return null;
 	}
 	
 }
